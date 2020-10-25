847fd2b945adf9aeb4a05d87e3b8eaa5c0966143
MiguelMSanchez <miguel.mchez@gmail.com>
2020-10-25 18:48:05 +0100

    src a java y pruebas en el código

--- javaScript/keepitsimple.js
+++ javaScript/keepitsimple.js
@@ -1,5 +1,33 @@
 (function(){
-    var list = document.getElementById("list"),
+    let list = document.getElementById("list"),
         newTask = document.getElementById("newTask"),
-        btnSubmit = document.getElementById("btnSubmit"),
+        btnSubmit = document.getElementsByClassName("btnSubmit");
+
+        //funciones
+    let addTask = function(){
+        console.log("addTask")
+    };
+    
+    let cheackInput = function(){
+        console.log("cheackInput")
+    };
+    
+    let delateTask = function(){
+        console.log("deleteTask")
+    };
+
+    //eventos
+    
+    //agregar tarea
+    btnSubmit.addEventListener("click", addTask,);
+
+    //Comprobar Input
+    newTask.addEventListener("click", cheackInput());
+
+    //Borrando elementos de la lista
+    for (let i = 0; i <= listjs.children.length -1; i++) {
+        list.children[i].addEventListener("click", delateTask);
+    }
+
 }());
+
