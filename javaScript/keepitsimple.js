b13a5e6792900343f562813c4c520157cc1c2f41
MiguelMSanchez <miguel.mchez@gmail.com>
2020-10-25 15:15:07 +0200

    Pruebas de botones y funciones

---
+++ javaScript/keepitsimple.js
@@ -0,0 +1,5 @@
+(function(){
+    var list = document.getElementById("list"),
+        newTask = document.getElementById("newTask"),
+        btnSubmit = document.getElementById("btnSubmit"),
+}());
