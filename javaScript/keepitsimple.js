25a60b7e9a0d62668d98c94de09af389970d7f97
MiguelMSanchez <miguel.mchez@gmail.com>
2020-11-01 12:35:45 +0100

    modificacion de logica

--- javaScript/keepitsimple.js
+++ javaScript/keepitsimple.js
@@ -4,33 +4,47 @@
         btnNuevaTarea = document.getElementById("btn-agregar");
 
 //Funciones
-    var agregarTarea = function(){
+    function agregarTarea(){
     var tarea = tareaInput.value,
         nuevaTarea = document.createElement("li"),
         enlace = document.createElement("a"),
-        contenido = document.createTextNode(tarea);
+        contenido = document.createTextNode(tarea); 
 
+    if(comprobarImput(tarea)){ 
+         //Agregamos el contenido al enlace
+         enlace.appendChild(contenido);
+        //Le establecemos un atributo href
+         enlace.setAttribute("href", "#");
+        // Agregamos el enlace a la nueva tarea (li)
+         nuevaTarea.appendChild(enlace);
+        // Agregamos la nueva tarea a la lista
+         lista.appendChild(nuevaTarea);
+
+         agregarEventoATareas(lista)
+
+    }
+   
+};
+
+function comprobarImput(tarea){
     if(tarea ===""){
         tareaInput.setAttribute("placeholder", "Agregue una nueva tarea");
         tareaInput.className = "error";
         return false;
-
-    }
-    //Agregamos el contenido al enlace
-    enlace.appendChild(contenido);
-    //Le establecemos un atributo href
-    enlace.setAttribute("href", "#");
-    // Agregamos el enlace a la nueva tarea (li)
-    nuevaTarea.appendChild(enlace);
-    // Agregamos la nueva tarea a la lista
-    lista.appendChild(nuevaTarea);
+    }else {
+        return true;
+    } 
 };
 
-var comprobarImput = function(){
+
+/*var eliminarTarea = function(){
+}; */
+
+function eliminarTarea(){
+    this.remove() // el this dentro de la funcion de un eventListener, es siempre el elemente que disparo el evento (en este caso, el elemento clickeado)
 };
 
-var eliminarTarea = function(){
-};
+
 
 //Eventos
 
@@ -40,11 +54,12 @@ btnNuevaTarea.addEventListener("click", agregarTarea);
 //Comprobar Input
 tareaInput.addEventListener("click", comprobarImput);
 
-//Borrando Elemehntos de la lista
-for (var i = 0; i <= lista.children.length -1; i++) {
-    lista.children[i].addEventListener("click", eliminarTarea);
-    
+//Agrega un event Listener a todas las tareas, que al clickearlas se ejecuta Eliminar tarea
+function agregarEventoATareas(lista){
+    for (var i = 0; i < lista.children.length; i++) {
+        lista.children[i].addEventListener("click", eliminarTarea);
+    }     
 }
-    
+  
 }());
 
